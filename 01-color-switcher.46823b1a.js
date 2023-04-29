6b84cea40667b49399c6efcb3e5d2fc1a79fa137
OscarPreciado <oapreciado@utp.edu.co>
2023-04-29 00:40:18 +0000

    Deploying to gh-pages from @ OscarPreciado/goit-js-hw-09@d26d5bf8d3ab2338a17496798cc82f1f42c7b45f 🚀

---
+++ 01-color-switcher.46823b1a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=(document.querySelector("body"),null);var r=function(){o=setTimeout((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),r()}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")};t.addEventListener("click",r),e.addEventListener("click",(function(){clearTimeout(o),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.46823b1a.js.map
